3e209e2c9d001b742d1b230ca474cd9bd7aab707
Bartłomiej Ochota <64443801+Supriaki@users.noreply.github.com>
2022-04-12 21:49:37 +0200

    Create admin_nav.php

---
+++ php_fun/admin_nav.php
@@ -0,0 +1,9 @@
+<div id="panel-menu"> 
+<nav class="panel-nav">
+  <ul>
+    <li><a href="index.php?strona=diagram">Diagramy</a></li>
+    <li><a href="index.php?strona=zarzadzanie">Zarządzanie uczniami</a></li>
+    <li><a href="index.php?strona=logooff">Wyloguj</a></li>
+  </ul>
+</nav>
+</div>
